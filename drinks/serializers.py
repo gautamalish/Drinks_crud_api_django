b5d054f7cbc7f66c1c2ed0103664952bddce9ae7
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-07-05 06:48:43 +0545

    django crud api creation completed

---
+++ drinks/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Drink
+class DrinkSerializer(ModelSerializer):
+    class Meta:
+        model=Drink
+        fields=['id','name','description']
+        
